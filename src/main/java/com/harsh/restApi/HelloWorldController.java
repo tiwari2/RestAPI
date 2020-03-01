0a3854e372156dace0d9c03ad40eba7a7ff5eec2
Prem <prem.tomar.gwl@gmail.com>
2020-03-01 22:12:14 +0530

    initial commit

---
+++ src/main/java/com/harsh/restApi/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.harsh.restApi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloWorldController {
+
+	
+	@GetMapping(path="/hello")
+	public String HelloWorld() {
+		return "Hello  World";
+	}
+	
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello  World");
+	}
+	
+	@GetMapping(path="/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World , %s" , name));
+	}
+}
